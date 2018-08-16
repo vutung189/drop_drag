7e32fce464b401a8138defcf771095677b8a71f3
Vu Tung <vutung1891995@gmail.com>
2018-08-16 21:35:22 +0700

    abc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DragulaService } from 'ng2-dragula';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+public MANY_ITEMS = 'MANY_ITEMS';
+  public many = ['The', 'possibilities', 'are', 'endless!'];
+  public many2 = ['Explore', 'them'];
+
+  subs = new Subscription();
+
+  public constructor(private dragulaService:DragulaService) {
+    this.subs.add(dragulaService.dropModel(this.MANY_ITEMS)
+      .subscribe(({ el, target, source, sourceModel, targetModel, item }) => {
+        console.log('dropModel:');
+        console.log(el);
+        console.log(source);
+        console.log(target);
+        console.log(sourceModel);
+        console.log(targetModel);
+        console.log(item);
+      })
+    );
+    this.subs.add(dragulaService.removeModel(this.MANY_ITEMS)
+      .subscribe(({ el, source, item, sourceModel }) => {
+        console.log('removeModel:');
+        console.log(el);
+        console.log(source);
+        console.log(sourceModel);
+        console.log(item);
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
+  }
+
 }
